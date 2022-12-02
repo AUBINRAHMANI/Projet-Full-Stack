8ade064ca726b897a94a3225c0b74ba3c4dabfc4
Yvann PONCE <yvann.ponce@gmail.com>
2022-12-02 16:05:46 +0100

    ajout des classes ObjectiveType et Game

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Game.java
@@ -0,0 +1,28 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class Game {
+
+    public Game() {
+
+    }
+
+    public Objective pickObjective() {
+        return null;
+    }
+
+    public Plot pickPlot() {
+        return null;
+    }
+
+    public boolean askToSetPlot(){
+        return false;
+    }
+
+    public Map askGetMap(){
+        return null;
+    }
+
+    public int play() {
+        return 0;
+    }
+}
