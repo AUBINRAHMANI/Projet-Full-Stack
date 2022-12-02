1d8a269da1c50f7fd7c91cca352c4ec0000432e5
Ziad-Bouhlel <ziad.bouhlel@gmail.com>
2022-12-02 16:04:16 +0100

    Objective

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Objetive.java
@@ -0,0 +1,12 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+
+public class Objetive {
+
+    public int getPoint(){
+        return 0;
+    }
+
+    public ObjectiveType type(){
+        return null;
+    }
+}
