0796c2ab5e4e3b91b5296c047fd1f140b55379e4
Yvann PONCE <yvann.ponce@gmail.com>
2022-12-02 20:58:15 +0100

    implementation des fcts

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/GameEngine.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/GameEngine.java
@@ -2,23 +2,40 @@ package fr.cotedazur.univ.polytech.startingpoint;
 
 public class GameEngine {
 
-    public GameEngine() {
+    private Deck<Objective> objectiveDeck_;
+    private Deck<Plot>      plotDeck_;
+    private Map             map_;
 
+    public GameEngine() {
+        objectiveDeck_  = new Deck<Objective>();
+        plotDeck_       = new Deck<Plot>();
+        map_             = new Map();
+    }
+
+    public GameEngine(Deck<Objective> objectiveDeck, Deck<Plot> plotDeck, Map map) {
+        objectiveDeck_  = objectiveDeck;
+        plotDeck_       = plotDeck;
+        map_            = map;
     }
 
     public Objective pickObjective() {
-        return new Objective();
+        return objectiveDeck_.getNextCard();
     }
 
     public Plot pickPlot() {
-        return new Plot();
+        return plotDeck_.getNextCard();
     }
 
     public boolean askToSetPlot( Position position ){
+        if(map_.isPlotFree(position)){
+            map_.setPlot(position);
+            return true;
+        }
         return false;
     }
 
     public Map GetMap(){
-        return null;
+        final Map mapCp = map_;
+        return mapCp;
     }
 }
