fba6c2d581977d68873268b55d3275b83185a10d
AUBINRAHMANI <aubin.rahmani@gmail.com>
2022-12-09 13:22:02 +0100

    First version of BOT

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Bot.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Bot.java
@@ -4,16 +4,22 @@ public class Bot {
 
 
 
-    public Bot(){
-
+    public Bot() {
     }
 
-    public void play(){
-
+    public void play(Game gameEngine, Map map) {
+        this.plotToSet(gameEngine, map);
     }
 
-    public void ChooseAction(){
+    ;
 
-    }
-    
+
+    public Plot plotToSet(Game gameEngine, Map map) {
+        if (map.isPlotFree(gameEngine.pickPlot()) && (gameEngine.askToSetPlot(this.plotToSet()))) {
+            gameEngine.play();
+        }
+
+
+        }
+
 }
