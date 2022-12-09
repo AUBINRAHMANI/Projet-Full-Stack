ccc70109dc04fe7115125d3a3aa367a1cda1602f
AUBINRAHMANI <aubin.rahmani@gmail.com>
2022-12-09 16:56:28 +0100

    Update Bot

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Bot.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Bot.java
@@ -1,24 +1,39 @@
 package fr.cotedazur.univ.polytech.startingpoint;
 
+import java.util.ArrayList;
+
 public class Bot {
+
+    private Map map= null;
 
 
 
     public Bot() {
     }
 
-    public void play(Game gameEngine, Map map) {
-        this.plotToSet(gameEngine, map);
+    public void play(Plot P) {
+       p.setPosition();
     }
 
     ;
 
 
-    public Plot plotToSet(Game gameEngine, Map map) {
-        if (map.isPlotFree(gameEngine.pickPlot()) && (gameEngine.askToSetPlot(this.plotToSet()))) {
-            gameEngine.play();
-        }
+    public void plotToSet(Game gameEngine, Map map) {
+         int i=0;
+         this.map=map.getMap();
+         Plot FIRST_PLOT= this.map.get(0);
+       // ArrayList<Plot> NewList = new ArrayList<>();
 
+        if(map.closestAvailableSpace(FIRST_PLOT!=null){
+            this.play(FIRST_PLOT);
+        }
+        else {
+            while((map.closestAvailableSpace(this.map.get(i)).size)==null){
+                i++;
+            }
+            this.play(this.map.get(i));
+
+             }
 
         }
 
