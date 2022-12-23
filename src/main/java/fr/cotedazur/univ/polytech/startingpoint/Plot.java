1c8d013a2935cb58a1054761abbe846452b30f65
Yacine <merioua72@gmail.com>
2022-12-23 19:45:15 +0100

    add bambou features

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/Plot.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Plot.java
@@ -4,13 +4,16 @@ public class Plot {
 
     private PlotType _plotType;
     private Position _position;
+    private int bambou;
     public Plot(PlotType plotType){
         _plotType = plotType;
         _position = null;
+        bambou=0;
     }
     public Plot(PlotType plotType, Position position){
         _plotType = plotType;
         _position = position;
+        bambou =0;
     }
 
     public void setPosition(Position _position) {
@@ -22,5 +25,19 @@ public class Plot {
     public Position getPosition() {
         return _position;
     }
+
+    public int getNumberOfBambou(){return this.bambou;}
+    public void growBambou() {
+        if (this.bambou < 4) {
+            this.bambou = bambou + 1;
+        }
+    }
+    public int eatBambou(){
+        if(this.bambou>0){
+            return this.bambou= bambou-1;
+        }
+        return 0;
+    }
+
 }
 
