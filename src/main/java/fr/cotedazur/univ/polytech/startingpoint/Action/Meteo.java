04c35e724db7f90f1213f0f8facb821c9eb42006
Yacine <merioua72@gmail.com>
2023-01-13 15:38:25 +0100

    add meteo clas #12

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/Action/Meteo.java
@@ -0,0 +1,16 @@
+package fr.cotedazur.univ.polytech.startingpoint.Action;
+
+public class Meteo {
+    public void drawSun(){
+
+    }
+    public void drawRain(){
+
+    }
+    public void drawCloud(){}
+    public void drawThunderstorm(){}
+    public void drawWind(){}
+    public void drawQuestionMark(){}
+
+
+}
