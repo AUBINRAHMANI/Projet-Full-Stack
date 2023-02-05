8f3d3b16db438e67550d3e4dc79e890f2d9bafc1
Ziad-Bouhlel <ziad.bouhlel@gmail.com>
2023-02-05 22:51:39 +0100

    debug PandaTest

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/PandaTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/PandaTest.java
@@ -1,10 +1,9 @@
 package fr.cotedazur.univ.polytech.startingpoint;
-
+import fr.cotedazur.univ.polytech.startingpoint.game.Game;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 class PandaTest {
 
     @Test
