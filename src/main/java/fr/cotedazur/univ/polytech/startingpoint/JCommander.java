9b357c1991528073e328944eae9fe81e11d49199
Yacine <merioua72@gmail.com>
2023-02-07 10:11:51 +0100

    begin of jcommander #35

---
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/JCommander.java
@@ -0,0 +1,9 @@
+package fr.cotedazur.univ.polytech.startingpoint;
+import com.beust.jcommander.Parameter;
+
+public class JCommander {
+    public static void main(final String[] args) {
+        final var options = new Options();
+        JCommander.
+    }
+}
