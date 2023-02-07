d055a692d4fc51924b462795df457b7b5bec139c
Yvann PONCE <yvann.ponce@gmail.com>
2023-02-07 10:11:40 +0100

    resolve test failed

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/ThousandGameLoop.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ThousandGameLoop.java
@@ -8,8 +8,8 @@ import java.util.logging.Level;
 public class ThousandGameLoop implements Loggeable {
 
     public static void main(String[] args) {
-        Loggeable.initLogger(Level.FINE);
-        for(int i=0; i<10000 ;++i){
+        Loggeable.initLogger(Level.FINEST);
+        for(int i=0; i<1000 ;++i){
             LOGGER.config("Game "+i);
             Game game = new Game(false);
             game.start();
