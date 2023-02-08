b6849ab968447206d3b2af48f42c64b5f519f48a
Yacine <merioua72@gmail.com>
2023-02-08 13:11:17 +0100

    add more test #39

--- src/main/java/fr/cotedazur/univ/polytech/startingpoint/ThousandGameLoop.java
+++ src/main/java/fr/cotedazur/univ/polytech/startingpoint/ThousandGameLoop.java
@@ -8,7 +8,7 @@ import java.util.logging.Level;
 public class ThousandGameLoop implements Loggeable {
 
     public static void main(String[] args) {
-        Loggeable.initLogger(Level.FINE);
+        Loggeable.initLogger(Level.CONFIG);
         for(int i=0; i<10000 ;++i){
             LOGGER.config("Game "+i);
             Game game = new Game(false);
