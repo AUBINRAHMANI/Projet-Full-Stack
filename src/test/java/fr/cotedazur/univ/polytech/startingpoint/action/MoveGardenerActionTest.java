86868885cf16c9559311a3dea4ba2a922589e9e6
Yacine <merioua72@gmail.com>
2023-02-09 22:27:15 +0100

    action test #39

--- src/test/java/fr/cotedazur/univ/polytech/startingpoint/action/MoveGardenerActionTest.java
+++ src/test/java/fr/cotedazur/univ/polytech/startingpoint/action/MoveGardenerActionTest.java
@@ -1,13 +1,13 @@
 package fr.cotedazur.univ.polytech.startingpoint.action;
 
 import fr.cotedazur.univ.polytech.startingpoint.GameEngine;
-import fr.cotedazur.univ.polytech.startingpoint.Gardener;
 import fr.cotedazur.univ.polytech.startingpoint.Position;
 import fr.cotedazur.univ.polytech.startingpoint.game.Game;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -32,5 +32,60 @@ class MoveGardenerActionTest {
         when(mockedGame.computeObjectivesGardener()).thenReturn(true);
         MoveGardenerAction moveGardenerAction = new MoveGardenerAction(null);
         assertTrue(moveGardenerAction.verifyObjectiveAfterAction(mockedGame));
+    }
+    @Test
+    void isActionMoveGardener() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertTrue(moveGardenerAction.isActionMoveGardener());
+    }
+
+    @Test
+    void isActionMovePanda() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertFalse(moveGardenerAction.isActionMovePanda());
+    }
+
+    @Test
+    void isActionPutPlot() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertFalse(moveGardenerAction.isActionPutPlot());
+    }
+
+    @Test
+    void isActionPickObjective() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertFalse(moveGardenerAction.isActionPickObjective());
+    }
+
+    @Test
+    void isActionRain() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertFalse(moveGardenerAction.isActionRain());
+    }
+
+    @Test
+    void isActionThunder() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertFalse(moveGardenerAction.isActionThunder());
+    }
+
+    @Test
+    void getPosition() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertEquals(moveGardenerAction.getPosition(),position);
+    }
+
+    @Test
+    void toType() {
+        Position position = new Position(2, 1);
+        MoveGardenerAction moveGardenerAction = new MoveGardenerAction(position);
+        assertEquals(moveGardenerAction.toType(), ActionType.MOVE_GARDENER);
     }
 }
